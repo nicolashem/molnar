b2c58609f1765aeadc1de8bfd74652f0434b664f
nicolash1989 <91009364+nicolash1989@users.noreply.github.com>
2022-02-20 23:12:01 +0100

    commit

--- p5/squares_2d.js
+++ p5/squares_2d.js
@@ -1,31 +1,47 @@
 
 let square = 50;
-let distance = 72;
+let distance = 70;
 let cellsX = 7;
 let cellsY = 10;
 let offset = -cellsX/2 * distance
 let sizes = [0, 0, square, square * 0.9, square * 0.8, square * 0.7, square * 0.6, square * 0.5, square * 0.4, square * 0.3, square * 0.2, square * 0.1];
-//let path;
-
-let slider1;
-let slider2;
-let slider3;
 
 
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(700, 950);
 
-  slider = createSlider(0, 255, 100);
+  slider = createSlider(0, 255, 255);
   slider.position(10, 10);
-  slider.style('width', '80px');
+  slider.style('width', '120px');
+  slider.style('background', 'red');
 
   slider2 = createSlider(0, 20, 10);
   slider2.position(10, 30);
-  slider2.style('width', '80px');
+  slider2.style('width', '120px');
 
   slider3 = createSlider(0, 20, 10);
   slider3.position(10, 50);
-  slider3.style('width', '80px');
+  slider3.style('width', '120px');
+
+  slider4 = createSlider(0, 100, 50);
+  slider4.position(10, 70);
+  slider4.style('width', '120px');
+
+  slider5 = createSlider(1, 15, 8);
+  slider5.position(10, 90);
+  slider5.style('width', '120px');
+
+  slider6 = createSlider(1, 20, 10);
+  slider6.position(10, 110);
+  slider6.style('width', '120px');
+
+  slider7 = createSlider(-200, 200, 0);
+  slider7.position(10, 130);
+  slider7.style('width', '120px');
+
+  slider8 = createSlider(-200, 200, 0);
+  slider8.position(10, 150);
+  slider8.style('width', '120px');
 }
 
 function draw() {
@@ -41,10 +57,23 @@ function draw() {
 
   let val3 = slider3.value();
 
+  let val4 = slider4.value();
+  distance = val4
+
+  let val5 = slider5.value();
+  cellsX = val5
+
+  let val6 = slider6.value();
+  cellsY = val6
+
+  let val7 = slider7.value();
+
+  let val8 = slider8.value();
 
   randomSeed(val3)
-  //console.log("hell")
 
+  translate(val7,val8)
+  
   for(let x = 0; x < cellsX; x++){
     for(let y = 0; y < cellsY; y++){
             
@@ -54,9 +83,6 @@ function draw() {
         for(let z = 0; z < sizes.length; z++) {
 
           let dice = Math.floor(random() * sizes.length);
-
-          //let diceStep = 3.25;
-
 
           let dice2 = random() * diceStep
           let dice3 = random() * diceStep
